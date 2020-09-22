858bfb81967dee66553d557828f2c3c22195790c
Eleonora Carletti <37311359+ele9996@users.noreply.github.com>
2020-09-22 10:51:28 +0200

    Update myLib.py

--- myLib.py
+++ myLib.py
@@ -1,1 +1,32 @@
 
+def nearest_neighbour(X, x):
+    euclidean = np.ones(X.shape[0]-1)
+    
+    additive = [None]*(1*X.shape[1])
+    additive = np.array(additive).reshape(1, X.shape[1])
+    k = 0
+    for j in range(0,X.shape[0]):
+        if np.array_equal(X[j], x) == False:
+            euclidean[k] = sqrt(sum((X[j]-x)**2))
+            k = k + 1
+    euclidean = np.sort(euclidean)
+    weight = rand.random()
+    while(weight == 0):
+        weight = rand.random()
+    additive = np.multiply(euclidean[:1],weight)
+    return additive
+    
+def SMOTE_100(X):
+    new = [None]*(X.shape[0]*X.shape[1])
+    new = np.array(new).reshape(X.shape[0],X.shape[1])
+    k = 0
+    for i in range(0,X.shape[0]):
+        additive = nearest_neighbour(X, X[i])
+        for j in range(0,1):
+            new[k] = X[i] + additive[j]
+            k = k + 1
+    return new 
+  
+  
+
+ 
